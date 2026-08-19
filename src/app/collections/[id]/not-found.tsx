2527e515eb410aa057389ffd1e325a8a4df78505
Aleksey Nakhimov <tj1993@bk.ru>
2026-08-19 11:51:31 +0300

    fix: collections page add not found

---
+++ src/app/collections/[id]/not-found.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import ImageNotFound from '@/shared/assets/images/not-found.png'
+
+export default function NotFound() {
+  return (
+    <div className="container not-found">
+      <Image
+        src={ImageNotFound}
+        alt="404"
+        width={500}
+        height={500}
+        loading="eager"
+      />
+      <h1>404 Not Found</h1>
+      <p>Could not find the requested collection.</p>
+      <Link href="/collections">Return to Collections</Link>
+    </div>
+  )
+}
